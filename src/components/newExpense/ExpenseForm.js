3ce08dd200e2dca5f4938a68366a92cb5bd94ed5
root <mopano>
2023-05-26 05:19:05 -0700

    addedMultiHandler

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -1,19 +1,43 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [title, setTitle] = useState("");
+    const [amount, setAmount] = useState("");
+    const [date, setDate] = useState("");
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.target.value);
+    };
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    };
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value);
+    };
+
     return (
         <form className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input
+                    type="number"
+                    min="0.01"
+                    step="0.01"
+                    onChange={amountChangeHandler}
+                />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2020-01-01" />
+                <input
+                    type="date"
+                    min="2020-01-01"
+                    onChange={dateChangeHandler}
+                />
             </div>
             <button type="submit">Submit</button>
         </form>
